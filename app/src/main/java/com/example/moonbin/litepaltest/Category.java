de6b338beb20aae6fc98ad722f13ee10a805b26a
Moonbin <1026310980@qq.com>
2019-09-18 21:41:12 +0800

    first commit

---
+++ app/src/main/java/com/example/moonbin/litepaltest/Category.java
@@ -0,0 +1,23 @@
+package com.example.moonbin.litepaltest;
+
+/**
+ * Created by Moonbin on 2019/8/1.
+ */
+
+public class Category {
+    private int id;
+    private String categoryName;
+    private int categoryCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+}
